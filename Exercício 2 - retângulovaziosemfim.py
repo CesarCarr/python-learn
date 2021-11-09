ebe8cb9403b667426f50955e1f8b0c388cda84c1
Cesar Carr <54749552+CesarCarr@users.noreply.github.com>
2021-11-09 15:24:30 -0300

    rectangle program
    prints a rectangle according to user's input

---
+++ Exercício 2 - retângulovaziosemfim.py
@@ -0,0 +1,27 @@
+﻿largura = int(input("Digite a largura de um retângulo: "))
+altura = int(input("Digite a altura de um retângulo: "))
+
+def linhavazia(x):
+    print("#",end="")
+    while x >= 3:
+        print(" ",end="")
+        x = x - 1
+    print("#")
+
+def Largura(i):
+    while i >= 1:
+        print("#", end="")
+        i = i - 1
+
+while altura >= 1 and largura >=1:
+    Largura(largura)
+    altura = altura - 1
+    print(end='\n')
+    while altura > 1:
+        linhavazia(largura)
+        altura = altura - 1
+    Largura(largura)
+    largura = 0
+
+print(end='\n')
+input("Aperte Enter para sair: ")
